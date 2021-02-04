9b6196271a80258602063690c944692cb5c2c2cc
Aurélien Bissey <Aurelien.Bissey@diiage.org>
2021-02-04 10:50:49 +0100

    Added methods authentification

---
+++ Diiage.DungeonCrawlerBackEnd.Business/AuthentificationService.cs
@@ -0,0 +1,39 @@
+﻿using Diiage.DungeonCrawlerBackEnd.Business.Contracts;
+using Diiage.DungeonCrawlerBackEnd.Entity;
+using Diiage.DungeonCrawlerBackEnd.Repository.Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diiage.DungeonCrawlerBackEnd.Business
+{
+    public class AuthentificationService : IAuthentificationService
+    {
+        private readonly IUserRepository _userRepository; 
+        /// <summary>
+        /// Allow register an user
+        /// </summary>
+        /// <param name="user">user we want to register</param>
+        public void Register(User user)
+        {
+            _userRepository.Add(user);
+        }
+        /// <summary>
+        /// Allow to reset the password of an user
+        /// </summary>
+        public void ResetPassword(User user, User newPassword)
+        {
+            _userRepository.ResetPassword(user, newPassword);
+        }
+
+        public void SignIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Diiage.DungeonCrawlerBackEnd.Repository/DbUserRepository.cs
+++ Diiage.DungeonCrawlerBackEnd.Repository/DbUserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Diiage.DungeonCrawlerBackEnd.Entity;
+using Diiage.DungeonCrawlerBackEnd.Entity.Context;
 using Diiage.DungeonCrawlerBackEnd.Repository.Contract;
 using Microsoft.EntityFrameworkCore.Query;
 using System;
@@ -11,61 +12,122 @@ namespace Diiage.DungeonCrawlerBackEnd.Repository
 {
     class DbUserRepository : IUserRepository
     {
+        /// <summary>
+        /// Allow to add a new user into the DB
+        /// <param name="entity">user we want to add</param>
         public void Add(User entity)
         {
-            throw new NotImplementedException();
+            using (var context = new DungeonCrawlerDbContext())
+            {
+                context.Set<User>().Add(entity);
+                context.SaveChanges();
+            }
         }
 
+        /// <summary>
+        /// Allow to add a list of new users
+        /// </summary>
+        /// <param name="entities">list of users we want to add</param>
         public void Add(IEnumerable<User> entities)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow to delete an user
+        /// </summary>
+        /// <param name="id">id of the user we want to delete</param>
         public void Delete(object id)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow to delete an user
+        /// </summary>
+        /// <param name="entityToDelete">user we want to delete</param>
         public void Delete(User entityToDelete)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow delete a list of users
+        /// </summary>
+        /// <param name="entities">list of users we want to delete</param>
         public void Delete(IEnumerable<User> entities)
         {
             throw new NotImplementedException();
         }
 
-        public IQueryable<User> FromSql(string sql, params object[] parameters)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Allow to get all users
+        /// </summary>
+        /// <returns>list of all users</returns>
         public IQueryable<User> GetAll()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow to get one user
+        /// </summary>
+        /// <param name="keyValues">id of the user we want</param>
+        /// <returns>the user uwser we want</returns>
         public User GetById(params object[] keyValues)
         {
             throw new NotImplementedException();
         }
 
-        public User GetFirstOrDefault(Expression<Func<User, bool>> predicate = null, Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null, Func<IQueryable<User>, IIncludableQueryable<User, object>> include = null, bool disableTracking = true)
+        /// <summary>
+        /// Allow to reset the password
+        /// </summary>
+        /// <param name="user">user we want to reset password</param>
+        public void ResetPassword(User user, User newPassword)
+        {
+            using (var context = new DungeonCrawlerDbContext())
+            {
+                user.UserPasswordHash = newPassword.UserPasswordHash;
+                user.UserPasswordSalt = newPassword.UserPasswordSalt;
+                context.Update<User>(user);
+                context.SaveChanges();
+            }
+        }
+
+        public void ResetPassword(User user)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<User> GetMuliple(Expression<Func<User, bool>> predicate = null, Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null, Func<IQueryable<User>, IIncludableQueryable<User, object>> include = null, bool disableTracking = true)
+        /// <summary>
+        /// Allow to connect an user
+        /// </summary>
+        public void SignIn()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow to disconnect an user
+        /// </summary>
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Allow to update user
+        /// </summary>
+        /// <param name="entity">user we want to update</param>
         public void Update(User entity)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Allow to update users
+        /// </summary>
+        /// <param name="entities">list of users wa want to update</param>
         public void Update(IEnumerable<User> entities)
         {
             throw new NotImplementedException();
