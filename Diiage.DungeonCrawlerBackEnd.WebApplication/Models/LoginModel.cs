ed3eb8769834ad2c9feae9e531d9138e8b7d53fb
Arthur Cuney <Arthur.Cuney@diiage.org>
2021-02-04 14:18:05 +0100

    Ajout du hub d'authentification

---
+++ Diiage.DungeonCrawlerBackEnd.WebApplication/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diiage.DungeonCrawlerBackEnd.WebApplication.Models
+{
+    public class LoginModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
+
+    }
+}
