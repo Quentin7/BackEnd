ed80b9c3ec96a55c0080b007ee986d8b4ae1480d
Arthur Cuney <Arthur.Cuney@diiage.org>
2021-02-17 13:27:50 +0100

    Https

--- Diiage.DungeonCrawlerBackEnd.WebApplication/Controllers/AuthentificationController.cs
+++ Diiage.DungeonCrawlerBackEnd.WebApplication/Controllers/AuthentificationController.cs
@@ -1,143 +1,48 @@
-﻿using Diiage.DungeonCrawlerBackEnd.Business.Contracts;
-using Diiage.DungeonCrawlerBackEnd.Entity;
-using Diiage.DungeonCrawlerBackEnd.WebApplication.Configuration;
-using Diiage.DungeonCrawlerBackEnd.WebApplication.Models.DTO.Requests;
-using Diiage.DungeonCrawlerBackEnd.WebApplication.Models.DTO.Responses;
-using Microsoft.AspNetCore.Identity;
+﻿using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Primitives;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Server;
 using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Diiage.DungeonCrawlerBackEnd.WebApplication.Controllers
 {
     public class AuthentificationController : Controller
     {
-        readonly private IAuthentificationService _authentificationService;
-        readonly private JwtConfig _jwtConfig;
-
-        public AuthentificationController( IAuthentificationService authentificationService, JwtConfig jwtConfig)
+        [HttpPost("~/connect/token"), Produces("application/json")]
+        public IActionResult Exchange(OpenIdConnectRequest connectionRequest)
         {
-            _authentificationService = authentificationService;
-            _jwtConfig = jwtConfig;
-        }
-
-        /// <summary>
-        /// Allow to register a new user
-        /// </summary>
-        /// <param name="user">user we want to register</param>
-        [HttpPost]
-        [Route("register")]
-        public RegistrationResponse Register(User user)
-        {
-            if (this.ModelState.IsValid)
+            if (connectionRequest.IsPasswordGrantType())
             {
-                if (_authentificationService.VerificationEMail(user.UserEMail) != new User())//wik
+                // Validate the user credentials.
+                // Note: to mitigate brute force attacks, you SHOULD strongly consider
+                // applying a key derivation function like PBKDF2 to slow down
+                // the password validation process. You SHOULD also consider
+                // using a time-constant comparer to prevent timing attacks.
+                if (connectionRequest.Username != "alice@wonderland.com" ||
+                    connectionRequest.Password != "P@ssw0rd")
                 {
-                    _authentificationService.Register(user);
-
-
-                    //_authentificationService.SignIn();
-                    GenerateJwtToken(user);
-
-
-                    return new RegistrationResponse()
-                    {
-                        Result = true,
-
-                    };
-
+                    return Forbid(OpenIddictServerDefaults.AuthenticationScheme);
                 }
-                else
-                {
-                    return new RegistrationResponse()
-                    {
-                        Result = false,
-                        Errors = new List<string>() 
-                        { 
-                            "Email already exist"
-                        }
-                    };
-                }
+                // Create a new ClaimsIdentity holding the user identity.
+                var identity = new ClaimsIdentity(
+                    OpenIddictServerDefaults.AuthenticationScheme,
+                    OpenIdConnectConstants.Claims.Name,
+                    OpenIdConnectConstants.Claims.Role);
+                // Add a "sub" claim containing the user identifier, and attach
+                // the "access_token" destination to allow OpenIddict to store it
+                // in the access token, so it can be retrieved from your controllers.
+                identity.AddClaim(OpenIdConnectConstants.Claims.Subject,
+                    "71346D62-9BA5-4B6D-9ECA-755574D628D8",
+                    OpenIdConnectConstants.Destinations.AccessToken);
+                identity.AddClaim(OpenIdConnectConstants.Claims.Name, "Alice",
+                    OpenIdConnectConstants.Destinations.AccessToken);
+                // ... add other claims, if necessary.
+                var principal = new ClaimsPrincipal(identity);
+                // Ask OpenIddict to generate a new token and return an OAuth2 token response.
+                return SignIn(principal, OpenIddictServerDefaults.AuthenticationScheme);
             }
-            return new RegistrationResponse()
-            {
-                Result = false,
-                Errors = new List<string>()
-                        {
-                            "Model invalid"
-                        }
-            };
-
-
+            throw new InvalidOperationException("The specified grant type is not supported.");
         }
-
-        /// <summary>
-        /// Allow to connect an user
-        /// </summary>
-        [HttpPost]
-        public void SignIn()
-        {
-            _authentificationService.SignIn();
-        }
-        
-        /// <summary>
-        /// Allow to disconnect an user
-        /// </summary>
-        [HttpPost]
-        public void Disconnect()
-        {
-            _authentificationService.SignOut();
-        }
-
-        /// <summary>
-        /// Allow to reset the password
-        /// </summary>
-        /// <param name="user"></param>
-        /// <param name="newPassword"></param>
-        [HttpPost]
-        public void ResetPassord(User user, User newPassword)
-        {
-            _authentificationService.ResetPassword(user, newPassword);
-        }
-
-        private string GenerateJwtToken(User user)
-        {
-            var identityUser = new IdentityUser()
-            {
-                Email = user.UserEMail,
-            };
-
-            var jwtTokenHandler = new JwtSecurityTokenHandler();
-
-            var key = Encoding.ASCII.GetBytes(_jwtConfig.Secret);
-
-            var tokenDescriptor = new SecurityTokenDescriptor() 
-            {
-                Subject = new ClaimsIdentity(new[] 
-                {
-                    new Claim(JwtRegisteredClaimNames.Sub, identityUser.Email),
-                    new Claim(JwtRegisteredClaimNames.Email, identityUser.Email),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                }),
-                Expires = DateTime.UtcNow.AddHours(4),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
-
-            };
-
-
-            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
-
-            var jwtToken = jwtTokenHandler.WriteToken(token);
-
-            return jwtToken;
-        }
-
-
     }
 }
